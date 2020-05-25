d456d2e5114f98f27e87c80bba2c78e6031bd92f
Sourav <souravsirvastava027@gmail.com>
2020-05-25 23:05:47 +0530

    medianOfArray updated

--- ib_medianOfArray.py
+++ ib_medianOfArray.py
@@ -1,10 +1,12 @@
-A= [ 0,23 ]
-B= []
-C = sorted(A + B)
-print(C)
-C.sort()
-if len(C) %2 ==0:
-    result = C[(len(C)//2)-1] + C[(len(C)//2)]
-    print(result/2)
-else:
-    print(C[len(C)/2])
+class Solution:
+    # @param A : tuple of integers
+    # @param B : tuple of integers
+    # @return a double
+    def findMedianSortedArrays(self, A, B):
+        C = sorted(A + B)
+        # C.sort()
+        if len(C) %2 ==0:
+            result = C[(len(C)//2)-1] + C[(len(C)//2)]
+            return float(result)/float(2)
+        else:
+            return C[len(C)/2]
